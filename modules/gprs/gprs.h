cedb506a74efe69a009fdd80180accf3ba9d2edc
adelarja <adelarja@gmail.com>
2024-08-18 23:45:32 -0300

    Added module

---
+++ modules/gprs/gprs.h
@@ -0,0 +1,12 @@
+#ifndef _GPRS_
+#define _GPRS_
+
+#define ACCESS_POINT_NAME "wap.gprs.unifon.com.ar" // Movistar
+#define APN_USER "wep"
+#define APN_PASSWORD "wep"
+
+#define PROVIDER_NAME "UNIFON"
+#define REMOTE_SERVER_PUBLIC_IP ""
+#define SOCKET_PORT "1111"
+
+#endif
