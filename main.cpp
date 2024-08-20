9c839eb5a022bb325e22601e0dbdfb0688991235
adelarja <adelarja@gmail.com>
2024-08-20 18:14:54 -0300

    Fixed issue with timer

--- main.cpp
+++ main.cpp
@@ -4,13 +4,20 @@
 #include "gprs.h"
 #include <mbed.h>
 
+#define SECOND 1000
+
+int tick_counter = 0;
+
 int main() {
     initRainGauge();
     initGprs();
     while (true) {
         updateRainMeasure();
         uartTask();
-        updateGprs();
+        if (tick_counter++ >= SECOND) {
+            updateGprs();
+            tick_counter = 0;
+        }
         thread_sleep_for(DELAY_TIME);
     }
 }
