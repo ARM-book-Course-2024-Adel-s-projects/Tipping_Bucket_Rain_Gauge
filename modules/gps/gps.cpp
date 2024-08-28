fa639f62dc7d21f837ce7058a9741f891020b20e
adelarja <adelarja@gmail.com>
2024-08-28 00:53:41 -0300

    Fixed issues

--- modules/gps/gps.cpp
+++ modules/gps/gps.cpp
@@ -1,47 +1,27 @@
 #include "gps.h"
 
-static void readString(char* str);
+static void displayInfo(void);
 
 static BufferedSerial gpsSerial(PG_14, PG_9, 4800);
-static TinyGPS gps;
+static TinyGPSPlus gps;
 
 void getGpsPosition(void) {
-    bool newData = false;
-    unsigned long chars;
-    unsigned short sentences, failed;
 
     if(gpsSerial.readable()) {
         char c = gpsSerial.read();
 
         if(gps.encode(c))
-            newData = true;
+            displayInfo();
     }
+}
 
-    if (newData) {
-        float flat, flon;
-        unsigned int sat;
-        unsigned int hdop;
-        unsigned long age;
-
-        gps.f_get_position(&flat, &flon, &age);
-
-        #ifdef LOG
+static void displayInfo(void) {
+    if (gps.location.isValid()) {
         char str[50];
-        flat = (flat == TinyGPS::GPS_INVALID_ANGLE) ? 0.0 : flat;
-        sprintf(str, "LAT=%f", flat);
-        logMessage(str);
+        double lat = gps.location.lat;
+        double lng = gps.location.lng;
 
-        flon = (flon == TinyGPS::GPS_INVALID_ANGLE) ? 0.0 : flon;
-        sprintf(str, "LON=%f", flon);
+        sprintf(str, "LAT=%lf, LON=%lf", lat, lng);
         logMessage(str);
-
-        sat = (gps.satellites() == TinyGPS::GPS_INVALID_SATELLITES) ? 0 : gps.satellites();
-        sprintf(str, "SAT=%d", sat);
-        logMessage(str);
-
-        hdop = (gps.hdop() == TinyGPS::GPS_INVALID_HDOP) ? 0 : gps.hdop();
-        sprintf(str, "HDOP=%d", hdop);
-        logMessage(str);
-        #endif
     }
 }
