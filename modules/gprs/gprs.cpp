db5e5dab8b93eda792b9f0c38ae1b297e1ca0f41
adelarja <adelarja@gmail.com>
2024-09-04 23:00:07 -0300

    Fixed rst command

--- modules/gprs/gprs.cpp
+++ modules/gprs/gprs.cpp
@@ -166,7 +166,7 @@ static void restartModule(void) {
     gprsModule.state = WAITING_FOR_MODULE_RESTART;
     
     gprsSerial.write(&ctrlZ, 1);
-    gprsSerial.write(RST, sizeof(RST));
+    gprsSerial.write(CFUN1_1, sizeof(CFUN1_1));
 
     #ifdef LOG
     logMessage("RESTARTING MODULE");
